7628594aeffc43d7700121aec77746ae67a33a97
SWork <swork91@mail.ru>
2021-05-27 01:03:53 +0300

    keyboard rework

--- keyboard.go
+++ keyboard.go
@@ -8,21 +8,56 @@ type KeyboardI interface {
 	Get() string
 }
 
-type InlineKeyboardMarkupT struct {
-	Buttons [][]*InlineKeyboardButtonT `json:"inline_keyboard"`
+type KeyboardButton struct {
+	Text string `json:"text,omitempty"`
+	Url  string `json:"url,omitempty"`
+	Data string `json:"callback_data,omitempty"`
 }
 
-type InlineKeyboardButtonT struct {
-	Text string `json:"text"`
-	Url  string `json:"url"`
-	Data string `json:"callback_data"`
+type KeyboardRow struct {
+	buttons []*KeyboardButton
 }
 
-func (k *InlineKeyboardMarkupT) Get() string {
+func (r *KeyboardRow) AddButton(text, url, data string) *KeyboardRow {
+	r.buttons = append(r.buttons,
+		&KeyboardButton{
+			Text: text,
+			Url:  url,
+			Data: data,
+		},
+	)
+
+	return r
+}
+
+func (r *KeyboardRow) MarshalJSON() ([]byte, error) {
+	return json.Marshal(r.buttons)
+}
+
+type InlineKeyboard struct {
+	Rows []*KeyboardRow `json:"inline_keyboard"`
+}
+
+func (k *InlineKeyboard) NewRow() *KeyboardRow {
+	row := &KeyboardRow{
+		buttons: []*KeyboardButton{},
+	}
+	k.Rows = append(k.Rows, row)
+
+	return row
+}
+
+func (k *InlineKeyboard) Get() string {
 	bytes, err := json.Marshal(k)
 	if err != nil {
 		return "{}"
 	}
 
 	return string(bytes)
+}
+
+func NewInlineKeyboard() *InlineKeyboard {
+	return &InlineKeyboard{
+		Rows: []*KeyboardRow{},
+	}
 }
