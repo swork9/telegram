aa270ce3e64b0c0570f7dc6ecee808909f32a818
SWork <swork91@mail.ru>
2021-05-26 23:10:55 +0300

    fix message id type

--- copyMessage.go
+++ copyMessage.go
@@ -5,12 +5,12 @@ import (
 	"strconv"
 )
 
-func (t *BotT) CopyMessage(chatID, fromChatID, messageID int64, disableNotification bool, newCaption string) (*MessageT, error) {
+func (t *BotT) CopyMessage(chatID, fromChatID int64, messageID uint64, disableNotification bool, newCaption string) (*MessageT, error) {
 	data := url.Values{}
 
 	data.Add("chat_id", strconv.FormatInt(chatID, 10))
 	data.Add("from_chat_id", strconv.FormatInt(fromChatID, 10))
-	data.Add("message_id", strconv.FormatInt(messageID, 10))
+	data.Add("message_id", strconv.FormatUint(messageID, 10))
 	data.Add("disable_notification", strconv.FormatBool(disableNotification))
 
 	if len(newCaption) > 0 {
--- forwardMessage.go
+++ forwardMessage.go
@@ -5,12 +5,12 @@ import (
 	"strconv"
 )
 
-func (t *BotT) ForwardMessage(chatID, fromChatID, messageID int64, disableNotification bool) (*MessageT, error) {
+func (t *BotT) ForwardMessage(chatID, fromChatID int64, messageID uint64, disableNotification bool) (*MessageT, error) {
 	data := url.Values{}
 
 	data.Add("chat_id", strconv.FormatInt(chatID, 10))
 	data.Add("from_chat_id", strconv.FormatInt(fromChatID, 10))
-	data.Add("message_id", strconv.FormatInt(messageID, 10))
+	data.Add("message_id", strconv.FormatUint(messageID, 10))
 	data.Add("disable_notification", strconv.FormatBool(disableNotification))
 
 	return t.sendRawMessage("forwardMessage", data)
